a3f46d89621a0ab8628898f7ea836c54cb4f8130
Michał Dolny <michal.dolny@gmail.com>
2017-03-17 21:40:32 +0100

    rozwiniecie skryptu

--- scripts.js
+++ scripts.js
@@ -2,11 +2,14 @@ var a = prompt('wpisz dowolną liczbę');
 var	b = prompt('wpisz dowolną liczbę');
 var value = (a * a) + (2 * a * b) - (b * b);
 
+console.log('Delta: ' + value);
 
 if (value < 0) {
 	console.log('wynik ujemny');
 } else if (value > 0) {
- 	console.log('wynik dodatni');
+	console.log('wynik dodatni');
 } else if (value === 0) { 
 	alert('wynik równy zero');
+} else {
+	console.log('nieprawidłowe dane - wynik nieokreślony');
 }
